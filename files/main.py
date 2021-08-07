5d18dc70d7eb342f01d69e7c0391ad286635c90f
juvanthomas <juvantk@gmail.com>
2021-08-08 00:34:02 +0530

    home page edited

--- files/main.py
+++ files/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "##########  Welcome to ask Budget  ###############"
+    return "##########      Welcome to ask Budget !!      ###############"
 
 
 
