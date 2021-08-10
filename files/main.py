4bca96a4cd77155fe15cae0fefb8b5da6b1e1b50
juvanthomas <47845423+juvanthomas@users.noreply.github.com>
2021-08-10 16:48:05 +0530

    Update main.py

--- files/main.py
+++ files/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "##########      Welcome to Budget !!      ###############"
+    return "##########      Welcome to  ask Budget !!      ###############"
 
 
 
