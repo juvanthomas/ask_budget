52120d944527459faefccd16c7823f70b6933beb
juvanthomas <juvantk@gmail.com>
2021-08-10 17:59:19 +0530

    landing page update

--- files/main.py
+++ files/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "##########      Welcome to  ask Budget 3.0!!      ###############"
+    return "##########      Welcome to  ask Budget 3.0!! updated      ###############"
 
 @app.route("/commit",methods=['POST'])
 def commit():
