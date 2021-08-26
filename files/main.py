9fc0903531b778592942592c4acd06149d4bdfb3
juvanthomas <47845423+juvanthomas@users.noreply.github.com>
2021-08-26 21:52:04 +0530

    Update main.py

--- files/main.py
+++ files/main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "##########      Welcome to  Ask Budget 5!    ###############"
+    return "##########      Welcome to  Ask Budget 5.1!    ###############"
 
 @app.route("/commit",methods=['POST'])
 def commit():
